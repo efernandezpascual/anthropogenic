d5d3fbc27d82fa42b3b73479ee3e4f5162da0783
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2023-11-16 18:30:49 +0100

    clasif vs expert

---
+++ src/clasif vs expert.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+read.csv("data/header-revised.csv", fileEncoding = "latin1") %>%
+  group_by(CODE, EUNIS2020) %>%
+  tally %>%
+  spread(EUNIS2020, n, fill = 0) %>%
+  write.csv("classification-vs-expert.csv", row.names = FALSE)
