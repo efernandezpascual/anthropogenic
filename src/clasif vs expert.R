0fc0cd137d61bdc2318cb0ae43672ccf2b07b1be
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2023-11-16 21:08:38 +0100

    clasif revision

--- src/clasif vs expert.R
+++ src/clasif vs expert.R
@@ -5,3 +5,11 @@ read.csv("data/header-revised.csv", fileEncoding = "latin1") %>%
   tally %>%
   spread(EUNIS2020, n, fill = 0) %>%
   write.csv("classification-vs-expert.csv", row.names = FALSE)
+
+library(tidyverse)
+
+read.csv("data/header-revised.csv", fileEncoding = "latin1") %>%
+  group_by(CODE, Alliance) %>%
+  tally %>%
+  spread(Alliance, n, fill = 0) %>%
+  write.csv("classification-vs-alliance.csv", row.names = FALSE)
